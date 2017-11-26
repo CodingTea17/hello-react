3eb8d0aa1ab5be1527648a96b50dc7b35a7da2ef
Dawson M <dawson.mortenson@gmail.com>
2017-11-26 12:22:16 -0800

    add a gitignore and setup the project to utilize babel to compile the JSX

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log("App.js is running");
+
+// JSX - JavaScript XML
+var template = <p>Babel is actually pretty cool</p>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
