a48382168da30c77c69477805b8a2fc697838e3a
Dawson M <dawson.mortenson@gmail.com>
2017-11-28 12:39:45 -0800

    create a counter example

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,43 +1,123 @@
-"use strict";
-
-console.log("App.js is running");
+'use strict';
 
 // JSX - JavaScript XML
+console.log("App.js is running");
+
+var app = {
+  title: 'Indecision App',
+  subtitle: 'It\'s raining',
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "div",
+  'div',
   null,
   React.createElement(
-    "p",
+    'h1',
     null,
-    "Babel is actually pretty cool"
+    app.title
   ),
   React.createElement(
-    "p",
+    'p',
     null,
-    "I'm a p tag too"
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Babel is actually pretty cool'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'I am a p tag too'
   )
 );
 
+var user = {
+  name: "Dawson Mortenson",
+  age: 22,
+  location: "PDX"
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
 var meTemplate = React.createElement(
-  "div",
+  'div',
   null,
   React.createElement(
-    "h1",
+    'h1',
     null,
-    "Dawson Morenson"
+    user.name ? user.name : 'Anonymous'
   ),
   React.createElement(
-    "p",
+    'p',
     null,
-    "Age: 22"
+    'Age: ',
+    user.age
   ),
-  React.createElement(
-    "p",
-    null,
-    "Location: PDX"
-  )
+  getLocation(user.location)
 );
+
+var count = 0;
+
+var increaseCount = function increaseCount() {
+  count += 1;
+  renderCounterApp();
+};
+var decreaseCount = function decreaseCount() {
+  count -= 1;
+  renderCounterApp();
+};
+var resetCount = function resetCount() {
+  count = 0;
+  renderCounterApp();
+};
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(meTemplate, appRoot);
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: increaseCount },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: decreaseCount },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: resetCount },
+      'reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,23 +1,70 @@
+// JSX - JavaScript XML
 console.log("App.js is running");
 
-// JSX - JavaScript XML
-var template = (
+const app = {
+  title: 'Indecision App',
+  subtitle: 'It\'s raining',
+  options: ['One', 'Two']
+}
+
+const template = (
   <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    {<p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>}
     <p>Babel is actually pretty cool</p>
-    <p>I'm a p tag too</p>
+    <p>I am a p tag too</p>
   </div>
 );
 
-var meTemplate = (
+const user = {
+  name: "Dawson Mortenson",
+  age: 22,
+  location: "PDX"
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+const meTemplate = (
   <div>
-    <h1>Dawson Morenson</h1>
-    <p>Age: 22</p>
-    <p>Location: PDX</p>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1>
+    <p>Age: {user.age}</p>
+    {getLocation(user.location)}
   </div>
 );
 
+let count = 0;
 
+const increaseCount = () => {
+  count += 1;
+  renderCounterApp();
+}
+const decreaseCount = () => {
+  count -= 1;
+  renderCounterApp();
+}
+const resetCount = () => {
+  count = 0;
+  renderCounterApp();
+}
 
-var appRoot = document.getElementById('app');
+const appRoot = document.getElementById('app');
 
-ReactDOM.render(meTemplate, appRoot);
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>+1</button>
+      <button onClick={decreaseCount}>-1</button>
+      <button onClick={resetCount}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+}
+
+renderCounterApp();
