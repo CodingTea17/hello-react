4ae5cdd61c1efd5c929158981300b9f5a8b2960e
Dawson M <dawson.mortenson@gmail.com>
2017-11-29 06:38:24 -0800

    update the visibility toggle to use class and state

--- src/build-it-visable.js
+++ src/build-it-visable.js
@@ -1,21 +1,53 @@
-const app = {
-  visable: false
-}
+// OLD CLASSLESS & STATELESS
+// const app = {
+//   visable: false
+// }
+//
+// const onToggleVisibility = () => {
+//   app.visable = !app.visable;
+//   render();
+// }
+//
+// const render = () => {
+//   const visableApp = (
+//     <div>
+//       <h1>Visibility Toggle</h1>
+//       <p>{app.visable ? 'Surprise! I\'m visable!' : '' }</p>
+//       <button onClick={onToggleVisibility}>{app.visable ? 'Click to Hide' : 'Click to Show'}</button>
+//     </div>
+//   );
+//   ReactDOM.render(visableApp, document.getElementById('app'))
+// }
+//
+// render();
 
-const onToggleVisibility = () => {
-  app.visable = !app.visable;
-  render();
-}
+class Visible extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onToggleVisibility = this.onToggleVisibility.bind(this);
+    this.state = {
+      visable: false
+    }
+  }
 
-const render = () => {
-  const visableApp = (
-    <div>
+  onToggleVisibility() {
+    console.log("I click");
+    this.setState((prevState) => {
+      return {
+        visable: !prevState.visable
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>
       <h1>Visibility Toggle</h1>
-      <p>{app.visable ? 'Surprise! I\'m visable!' : '' }</p>
-      <button onClick={onToggleVisibility}>{app.visable ? 'Click to Hide' : 'Click to Show'}</button>
-    </div>
-  );
-  ReactDOM.render(visableApp, document.getElementById('app'))
+      <p>{this.state.visable ? 'Surprise! I\'m visable!' : '' }</p>
+      <button onClick={this.onToggleVisibility}>{this.state.visable ? 'Click to Hide' : 'Click to Show'}</button>
+     </div>
+    );
+  }
 }
 
-render();
+ReactDOM.render(<Visible />, document.getElementById('app'));
